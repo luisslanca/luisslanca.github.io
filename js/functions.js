4a0fd222a167db6033dc1f583906a7c3f3a04db2
Luís Lança <luisanpralanca@gmail.com>
2018-01-17 17:19:51 -0200

    Arrumando formulário

--- js/functions.js
+++ js/functions.js
@@ -83,15 +83,4 @@ $("document").ready(function($){
 //WOW ANIMATE
 new WOW().init();
 
-$(document).ready(function(){
-    $("#hamburger").click(function(){
-        $("#menu").fadeIn();
-		
-    });
-});
-
-$(document).ready(function(){
-    $("#close").click(function(){
-        $("#menu").fadeOut();
-    });
-});
+$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
